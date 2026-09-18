241e6ec2db1ab5b5d259c38e3ad13714b1dee134
Giri Babu Bojanapu <giribabubojanapu123@gmail.com>
2026-09-18 11:40:56 +0530

    feat: add practice project files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
